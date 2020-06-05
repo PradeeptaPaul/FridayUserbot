ded5a7a3f35335fab8e3c8354f4d4ef90b5e627a
PradeeptaPaul <54322344+PradeeptaPaul@users.noreply.github.com>
2020-06-05 13:50:42 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -14,19 +14,19 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**MY STATUS** \n`FRIDAY IS:` **✅ Alive**\n\n"
-                     "`🌹TELETHON VERSION:` **6.0.9**\n`🐍Python:` **3.7.4**\n"
-                     "`DATABASE STATUS:` **WORKING FINE 🙂**\n\n`SYSTEM is 🔥!, NO malacious activity detected[✓]\n`"
-                     "________________________________________________________________________________/n"
-                     "`CURRECT BOT LOCATION:` **NEW YORK,UNITED STATES**\n"
-                     "`$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+                     "`🌹TELETHON VERSION:` **6.0.9**✅\n`🐍Python:` **3.7.4**✅\n"
+                     "`DATABASE STATUS:` **WORKING FINE 🙂**\n\n`SYSTEM is 🔥!, NO malacious activity detected✅\n`"
+                     "/🔘🔸🔸🔸🔸🔸🔸🔸🔸🔸🔸🔸🔸🔸🔸🔸🔸🔘n"
+                     "`CURRECT BOT LOCATION:NEW YORK, UNITED STATES**\n"
+                     "`🔻🔺🔻🔺🔻🔺🔻🔺🔻🔺🔻🔺🔻🔺🔻🔺🔻\n"
                      "`IP:` **3.82.66.232**\n"
-                     "`SYSTEM TYPE:`👑 LINUX\n"
+                     "`SYSTEM TYPE:`👑 LINUX✅\n"
                      "`BASED ON:`🧬JARVIS Works❤️\n"
                      "`SATELLITE🛰️:` ⚡TONY STARK SAT-2⚡\n"
                      "`MADE USING:` 🐍PYTHON\n"
                      f"`MY BOSS`: {DEFAULTUSER}☣️\n\n"
                      "AT YOUR SERVICE ,MY BOSS\n\n"
-                     "`MAINTAINER: TONY STARK...aka (ROBERT DOWNEY.Jr)[✓] \n\n"
+                     "`MAINTAINER: TONY STARK...aka (ROBERT DOWNEY.Jr)✅ \n\n"
                      )
     
                      
